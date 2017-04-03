104294709b889a5d629c67f6326038b732516c7e
alex <alex.nitter@web.de>
2017-04-03 20:16:50 +0200

    Namenskonvention angepasst

--- src/test/java/binBeats/BinBeatsPlayerTest.java
+++ src/test/java/binBeats/BinBeatsPlayerTest.java
@@ -3,36 +3,19 @@ package test.java.binBeats;
 
 import javax.sound.sampled.LineUnavailableException;
 import org.junit.Test;
+
+import main.java.binBeats.lib.BinBeat;
 import main.java.binBeats.lib.BinBeatsPlayer;
 
 public class BinBeatsPlayerTest {
 
 	@Test
-	public void LinearBinBeatsTest() throws LineUnavailableException, InterruptedException {
+	public void binBeatsPlayer_playStop_test() throws LineUnavailableException, InterruptedException {
+		BinBeat binBeat = new BinBeat(440, 460);
 		BinBeatsPlayer player = new BinBeatsPlayer();
-		player.setTraegerFrequenz(440);
-		player.setDifferenzFrequenz(460);
+		player.setBinBeat(binBeat);
 		player.play();
-		Thread.sleep(300000);
-		player.stop();
-	}
-	
-	@Test
-	public void GradientBinBeatsTest() throws LineUnavailableException, InterruptedException {
-		int traegerFrequenz = 440;
-		int differenzFrquenz = 440;
-		
-		BinBeatsPlayer player = new BinBeatsPlayer();
-		player.setTraegerFrequenz(traegerFrequenz);
-		player.setDifferenzFrequenz(differenzFrquenz);
-		player.play();
-		
-		while(differenzFrquenz <= traegerFrequenz + 30) {
-			Thread.sleep(2000);
-			differenzFrquenz++;
-			player.setDifferenzFrequenz(differenzFrquenz);
-		}
-		
+		Thread.sleep(60000);
 		player.stop();
 	}
 }
