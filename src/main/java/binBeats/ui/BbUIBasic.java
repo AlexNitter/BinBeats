5607c00bcdafcd95d1f264b0e200e001e92670b0
Magnus <Magnus@Magnus-1070.lan>
2017-04-10 17:29:10 +0200

    UI now allows decimal fractions for carrier frequency

--- src/main/java/binBeats/ui/BbUIBasic.java
+++ src/main/java/binBeats/ui/BbUIBasic.java
@@ -100,6 +100,7 @@ public class BbUIBasic {
 			// TODO: Popup
 			e1.printStackTrace();
 		}
+		binBeatValidator = new BinBeatValidator();
 		// TODO: Demo BinBeat
 		playerBinBeat = new BinBeat(432, 7);
 		
@@ -147,12 +148,13 @@ public class BbUIBasic {
 		panelPlayer.add(lblPlayerCarrierFrequency, "cell 1 1,alignx trailing");
 		
 		JFormattedTextField formattedTextFieldPlayerCarrier = new JFormattedTextField(numberFormatterEn);
+		formattedTextFieldPlayerCarrier.setHorizontalAlignment(SwingConstants.RIGHT);
 		formattedTextFieldPlayerCarrier.setValue(playerBinBeat.getCarrierFrequency());
 		formattedTextFieldPlayerCarrier.setToolTipText("Define the pitch of the carrier tone");
 		JSlider sliderPlayerCarrier = new JSlider();
 		sliderPlayerCarrier.setToolTipText("Define the pitch of the carrier tone");
 		
-		formattedTextFieldPlayerCarrier.setColumns(4);
+		formattedTextFieldPlayerCarrier.setColumns(7);
 		panelPlayer.add(formattedTextFieldPlayerCarrier, "flowx,cell 2 1,alignx left");
 		
 		sliderPlayerCarrier.setMinimum(20);
@@ -175,6 +177,7 @@ public class BbUIBasic {
 		panelPlayer.add(lblPlayerBeatFrequency, "cell 1 3,alignx trailing");
 		
 		JFormattedTextField formattedTextFieldPlayerBeatFreq = new JFormattedTextField(numberFormatterEn);
+		formattedTextFieldPlayerBeatFreq.setHorizontalAlignment(SwingConstants.RIGHT);
 		formattedTextFieldPlayerBeatFreq.setValue(playerBinBeat.getBeatFrequency());
 		String beatFreqTooltip = "<html>Define the frequency of the binaural beat"
 				+ "<ul>"
@@ -187,7 +190,7 @@ public class BbUIBasic {
 				+ "<li><strong>21 - 30</strong> - <em>High Beta</em> - stress</li>"
 				+ "</ul></html>";
 		formattedTextFieldPlayerBeatFreq.setToolTipText(beatFreqTooltip);
-		formattedTextFieldPlayerBeatFreq.setColumns(4);
+		formattedTextFieldPlayerBeatFreq.setColumns(7);
 		panelPlayer.add(formattedTextFieldPlayerBeatFreq, "flowx,cell 2 3,alignx left");
 		
 		JLabel lblPlayerBeatFreqHz = new JLabel("Hz");
@@ -213,10 +216,11 @@ public class BbUIBasic {
 		panelPlayer.add(lblPlayerBeatVolume, "cell 1 5,alignx trailing");
 		
 		JFormattedTextField formattedTextFieldPlayerBeatVol = new JFormattedTextField();
+		formattedTextFieldPlayerBeatVol.setHorizontalAlignment(SwingConstants.RIGHT);
 		formattedTextFieldPlayerBeatVol.setValue(playerBinBeat.getVolume());
 		panelPlayer.add(formattedTextFieldPlayerBeatVol, "flowx,cell 2 5");
 		formattedTextFieldPlayerBeatVol.setToolTipText("Define binaural beat volume");
-		formattedTextFieldPlayerBeatVol.setColumns(4);
+		formattedTextFieldPlayerBeatVol.setColumns(7);
 		
 		JLabel labelPlayerBeatVolume_0 = new JLabel("0");
 		panelPlayer.add(labelPlayerBeatVolume_0, "cell 0 6,alignx right");
@@ -248,9 +252,10 @@ public class BbUIBasic {
 		btnPlayerPlay.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				// collect data from UI in playerBinBeat
-				playerBinBeat.setCarrierFrequency((float)formattedTextFieldPlayerCarrier.getValue());
-				playerBinBeat.setBeatFrequency((float)formattedTextFieldPlayerBeatFreq.getValue());
-				playerBinBeat.setVolume((float)formattedTextFieldPlayerBeatVol.getValue());
+				// cannot use the fields' value() function because moving a slider will fill the field with int
+				playerBinBeat.setCarrierFrequency(Float.parseFloat(formattedTextFieldPlayerCarrier.getText()));
+				playerBinBeat.setBeatFrequency(Float.parseFloat(formattedTextFieldPlayerBeatFreq.getText()));
+				playerBinBeat.setVolume(Float.parseFloat(formattedTextFieldPlayerBeatVol.getText()));
 				binBeatsPlayer.setBinBeat(playerBinBeat);
 				try {
 					binBeatsPlayer.play();
@@ -264,11 +269,10 @@ public class BbUIBasic {
 		// Link carrier slider and text field
 		formattedTextFieldPlayerCarrier.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				// TODO: maybe change Attributes in BinBeatValidator class to int to avoid unnecessary typecasting
-				int value = checkValidity(formattedTextFieldPlayerCarrier.getText(), "\\d+", 4, (int)binBeatValidator.getCarrierFrequencyMin(), (int)binBeatValidator.getCarrierFrequencyMax());
+				float value = checkValidity(formattedTextFieldPlayerCarrier.getText(), "\\d+(\\.\\d*)?", 7, binBeatValidator.getCarrierFrequencyMin(), binBeatValidator.getCarrierFrequencyMax());
 				if (value > -1) {
+					sliderPlayerCarrier.setValue(Math.round(value));
 					formattedTextFieldPlayerCarrier.setValue(value);
-					sliderPlayerCarrier.setValue(value);
 				}
 			}
 		});
@@ -281,28 +285,15 @@ public class BbUIBasic {
 		
 		/* Link beat frequency slider and text field
 		 * Note: JSlider works with integers.
-		 * As we want to be able to change values by .1 we divide by 10.
+		 * As we want to be able to change values by .1 we multiply by 10.
 		 */
 		formattedTextFieldPlayerBeatFreq.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				String typedBeatFreq = formattedTextFieldPlayerBeatFreq.getText();
-				if(!typedBeatFreq.matches("\\d+(\\.\\d?)?")) {
-					return;
+			public void actionPerformed(ActionEvent arg0) {				
+				float value = checkValidity(formattedTextFieldPlayerBeatFreq.getText(), "\\d+(\\.\\d*)?", 5, binBeatValidator.getBeatFrequencyMin(), binBeatValidator.getBeatFrequencyMax());
+				if (value > -1){
+					sliderPlayerBeatFreq.setValue((int)(value*10));
+					formattedTextFieldPlayerBeatFreq.setValue(value);
 				}
-				float fieldValue = Float.parseFloat(typedBeatFreq);
-				int sliderValue = (int)(fieldValue*10);
-				// Check for correct BinBeat specifications and change if necessary
-				float beatFrequencyMin = binBeatValidator.getBeatFrequencyMin();
-				float beatFrequencyMax = binBeatValidator.getBeatFrequencyMax();
-				if (sliderValue < beatFrequencyMin*10) {
-					sliderValue = (int)(beatFrequencyMin*10);
-					fieldValue = beatFrequencyMin;
-				} else if (sliderValue > beatFrequencyMax*10) {
-					sliderValue = (int)(beatFrequencyMax*10);
-					fieldValue = beatFrequencyMax;
-				}
-				formattedTextFieldPlayerBeatFreq.setValue(fieldValue);
-				sliderPlayerBeatFreq.setValue(sliderValue);
 			}
 		});
 		sliderPlayerBeatFreq.addChangeListener(new ChangeListener() {
@@ -315,10 +306,10 @@ public class BbUIBasic {
 		// Link beat volume field with slider
 		formattedTextFieldPlayerBeatVol.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				int value = checkValidity(formattedTextFieldPlayerBeatVol.getText(), "\\d+", 3, (int)binBeatValidator.getVolumeMin(), (int)binBeatValidator.getVolumeMax());
+				float value = checkValidity(formattedTextFieldPlayerBeatVol.getText(), "\\d+", 3, binBeatValidator.getVolumeMin(), binBeatValidator.getVolumeMax());
 				if (value > -1) {
 					formattedTextFieldPlayerBeatVol.setValue(value);
-					sliderPlayerBeatVol.setValue(value);
+					sliderPlayerBeatVol.setValue((int)value);
 				}
 			}
 		});
@@ -347,12 +338,12 @@ public class BbUIBasic {
 	 * @param upperBound upper bound to be checked against
 	 * @return the checked and corrected value or -1 if not a valid number
 	 */
-	private int checkValidity(String typedValue, String regex, int maxLength, int lowerBound, int upperBound){
+	private float checkValidity(String typedValue, String regex, int maxLength, float lowerBound, float upperBound){
 		// Check if typed value is a valid number
 		if (!typedValue.matches(regex) || typedValue.length() > maxLength) {
 			return -1;
 		}
-		int value = Integer.parseInt(typedValue);
+		float value = Float.parseFloat(typedValue);
 		// Check for correct specifications and change if necessary
 		if (value < lowerBound) {
 			value = lowerBound;
